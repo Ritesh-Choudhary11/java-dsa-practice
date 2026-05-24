ecb22c09b287a2bb04f701ab95db6c5a2b1eabee
Ritesh <choudharykrritesh123@gmail.com>
2026-05-24 21:17:38 +0530

    leetcode238 prod of an array

---
+++ src/prodofarray2.java
@@ -0,0 +1,25 @@
+public class prodofarray2 {
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4};
+        int brr[]=new int[arr.length];
+        int n=arr.length;
+        int prod;
+        for(int i=0;i<n;i++){
+            prod=1;
+            for(int j=0;j<n;j++){
+                if(i==j){
+                    continue;
+                }
+                prod=prod*arr[j];
+
+            }
+            brr[i]=prod;
+
+        }
+        for(int i=0;i<n;i++){
+            System.out.println(brr[i]+" ");
+        }
+
+
+    }
+}
