08ed9050c60837e8d0c32916e5c94d513708497a
Ritesh <choudharykrritesh123@gmail.com>
2026-06-14 21:18:10 +0530

    check even odd using bitwise opperator

---
+++ src/Bitwiseoperator.java
@@ -0,0 +1,12 @@
+public class Bitwiseoperator {
+    public static void main(String[] args) {
+        int n=4;
+            if((n&1)==0){
+                System.out.println("is a even no");
+            }
+            else{
+                System.out.println("not even");
+            }
+
+    }
+}
