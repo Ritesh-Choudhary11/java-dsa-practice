8338d52b53a57e792ecd6eea71544a120835d300
Ritesh <choudharykrritesh123@gmail.com>
2026-07-28 21:31:46 +0530

    just basics done for oopjL

---
+++ src/oopsJ.java
@@ -0,0 +1,130 @@
+
+import java.util.Scanner;
+public class oopsJ {
+        public static void main(String []args){
+            Scanner sc=new Scanner(System.in);
+
+            //Q1
+            byte a=6;
+            short b=12;
+            int c=240;
+            long d=360;
+            float e=9.9f;
+            double f=30d;
+            char x= '&';
+            boolean z=true;
+            System.out.println("Byte="+a);
+            System.out.println("short="+b);
+            System.out.println("integar="+c);
+            System.out.println("long="+d);
+            System.out.println("float="+e);
+            System.out.println("double="+f);
+            System.out.println("character="+x);
+            System.out.println("boolean="+z);
+
+
+
+            //Q4
+//
+//            System.out.print("Enter a number: ");
+//            int n = sc.nextInt();
+//
+//            int original =n;
+//            int rev = 0;
+//
+//            while(n>0) {
+//                int rem=n%10;
+//                rev=rev*10+rem;
+//                n=n/10;
+//            }
+//            if(original==rev) {
+//                System.out.println("Palindrome");
+//            } else {
+//                System.out.println("Not Palindrome");
+//            }
+//        }
+//    }
+
+
+
+
+
+    //Q3
+//    int day= sc.nextInt();
+//	 switch(day){
+//        case 1:
+//            System.out.println("Monday");
+//            break;
+//
+//        case 2:
+//            System.out.println("tuesday");
+//            break;
+//
+//        case 3:
+//            System.out.println("wednesday");
+//            break;
+//
+//
+//        case 4:
+//            System.out.println("thursday");
+//            break;
+//
+//        case 5:
+//            System.out.println("friday");
+//            break;
+//
+//        case 6:
+//            System.out.println("saturday");
+//            break;
+//
+//        case 7:
+//            System.out.println("sunday");
+//            break;
+//
+//
+//
+//    }
+
+
+
+
+
+    //Q2
+//    int Marks= sc.nextInt();
+//	if(Marks>=90){
+//        System.out.println("your grade is O");
+//    }
+//
+//		else if(Marks>=80){
+//        System.out.println("your grade is E");
+//    }
+//
+//			else if(Marks>=70){
+//        System.out.println("your grade is A");
+//    }
+//
+//			else if(Marks>=60){
+//        System.out.println("your grade is B");
+//    }
+//
+//			else{
+//        System.out.println("The student is fail");
+//    }
+
+
+
+
+
+
+
+//System.out.println("my name is Ritesh choudhary");
+//System.out.println("my roll no is 2506024");
+//System.out.println("I am from IT Branch");
+
+
+
+
+}
+}
+
+
